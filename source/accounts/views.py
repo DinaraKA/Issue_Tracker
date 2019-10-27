f6b462149b321c62fb423fc2c52738948334def0
DinaraKA <d.karybekova@gmail.com>
2019-10-27 18:24:53 +0600

    Added register view in accounts views

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from accounts.forms import UserCreationForm
 
 
 def login_view(request):
@@ -21,8 +24,23 @@ def login_view(request):
             context['has_error'] = True
     return render(request, 'login.html', context=context)
 
-
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('webapp:index')
+
+def register_view(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form':form})
+    elif request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = User(username=form.cleaned_data['username'])
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('webapp:index')
+        else:
+            return render(request, 'register.html', {'form':form})
 
