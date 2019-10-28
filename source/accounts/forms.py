a0f73870d42e92f6c72ec6f66e920f66029e23c4
DinaraKA <d.karybekova@gmail.com>
2019-10-28 14:15:09 +0600

    Password length changed

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -7,9 +7,9 @@ class UserCreationForm(forms.Form):
     first_name = forms.CharField(max_length=100, label='First name', required=False)
     last_name = forms.CharField(max_length=100, label='Last name', required=False)
     username = forms.CharField(max_length=100, label='Username', required=True)
-    password = forms.CharField(max_length=100, label='Password', required=True,
+    password = forms.CharField(max_length=20, min_length=8, label='Password', required=True,
                                widget=forms.PasswordInput)
-    password_confirm = forms.CharField(max_length=100, label='Password Comfirm', required=True,
+    password_confirm = forms.CharField(max_length=20, min_length=8, label='Password Comfirm', required=True,
                                widget=forms.PasswordInput)
     email = forms.EmailField(label='Email', required=True)
 
@@ -36,15 +36,17 @@ class UserCreationForm(forms.Form):
 
     def clean(self):
         super().clean()
-        password_1 = self.cleaned_data['password']
-        password_2 = self.cleaned_data['password_confirm']
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
-        if password_1 != password_2:
-            raise ValidationError('Passwords do not match',
-                                  code='passwords_do_not_match')
+        password_1 = self.cleaned_data['password']
+        password_2 = self.cleaned_data['password_confirm']
+
         if not first_name and not last_name:
             raise ValidationError('First name or last name should be filled',
                                   code='no_first_and_last_name')
+        if password_1 != password_2:
+            raise ValidationError('Passwords do not match',
+                                  code='passwords_do_not_match')
+
         return self.cleaned_data
 
