22321d311394d1f26cf08e650b1ea8b75f2fc681
DinaraKA <d.karybekova@gmail.com>
2019-11-03 14:05:29 +0600

    Added model Profile and registered it in admin

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,11 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 from uuid import uuid4
 
 
 class Token(models.Model):
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE,
-                             verbose_name='user', related_name='registration_token')
+                             verbose_name='User', related_name='registration_token')
     token=models.UUIDField(verbose_name='token', default=uuid4)
 
     def __str__(self):
         return str(self.token)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='User')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Avatar')
+    about_user = models.TextField(max_length=2000, null=True, blank=True, verbose_name='About User')
+    github = models.URLField(null=True, blank=True, verbose_name='GitHub')
+
+
+    def __str__(self):
+        return self.user.get_full_name() + "'s Profile"
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
